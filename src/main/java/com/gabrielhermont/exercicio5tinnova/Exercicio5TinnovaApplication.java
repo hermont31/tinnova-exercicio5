cf38a1cb25be26407899a0b63796b87b6fb912fe
Gabriel Hermont <gabriel13hermont@gmail.com>
2022-08-16 15:38:15 -0300

    Projeto Criado

---
+++ src/main/java/com/gabrielhermont/exercicio5tinnova/Exercicio5TinnovaApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielhermont.exercicio5tinnova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercicio5TinnovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercicio5TinnovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielhermont/exercicio5tinnova/Exercicio5TinnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielhermont.exercicio5tinnova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercicio5TinnovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
